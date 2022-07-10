10eb821c2761efed519f774b821b3ebebc847d8e
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-10 18:25:03 +0700

    Hoàn thiện phần Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Service";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonials/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import "./index.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
