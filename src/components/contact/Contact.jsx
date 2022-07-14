7202d5889e88b8b96fba33cb947140d22ddc6a8f
NgoTanKhoa21042001 <Kakashi2104@gmail.com>
2022-07-14 20:49:16 +0700

    Updated Code

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__icon" />
+            <h4>Email</h4>
+            <h5>khoantgcd191215@fpt.edu.vn</h5>
+            <a href="mailto:kakashi21042001@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__icon" />
+            <h4>Messenger</h4>
+            <h5>ngotankhoaportfolio</h5>
+            <a href="https://www.facebook.com/khoa.tan.1654/">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__icon" />
+            <h4>WhatsApp</h4>
+            <h5>+84 3555 2021</h5>
+            <a href="http://api.whatsapp.com">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTION */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          ></input>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            required
+          ></input>
+          <textarea
+            name="password"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" class="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,54 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Footer.css";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testomonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/">
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com/khoatann214/">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com/MrKhoa98947757">
+          <FaTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
